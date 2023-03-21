f140b2df40c68c4c10a28ec54dfb50604b12e3e7
aldios <kynick@mail.ru>
2023-03-21 21:31:32 +0300

    Удалены ненужные комментарии

--- src/MenuExecutor.java
+++ src/MenuExecutor.java
@@ -63,13 +63,3 @@ public class MenuExecutor {
 
 
 
-
-
-
-//        //----------------------
-//        String treeName = "MainMenu";
-//        String[] tree = {"ChooseProgram", "Settings", "Info", "Back"};
-//
-//        String name = "ModularSolver\nГлавное меню";
-//        String[] possibleOptions = {"Выбрать программу", "Настройки", "Информация", "Назад"};
-//        //------------------
